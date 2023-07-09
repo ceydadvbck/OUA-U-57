e5cfec4f8de03067a0662f9791c21db706b19838
Ceyda Devebacak <ceydadevebacak@gmail.com>
2023-07-09 21:45:04 +0300

    Enemys still need work

--- Assets/Ceyda's Scene/Scripts/PlayerManager.cs
+++ Assets/Ceyda's Scene/Scripts/PlayerManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerManager: MonoBehaviour
+/* Keeps track of the player */
+
+public class PlayerManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	#region Singleton
+
+	public static PlayerManager instance;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	#endregion
+
+	public GameObject player;
+
+	public void KillPlayer()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
 }
