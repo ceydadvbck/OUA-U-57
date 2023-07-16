e07b29359e07237bdfc1b9d2ce5fcd39f1bf6650
Ceyda Devebacak <ceydadevebacak@gmail.com>
2023-07-17 02:09:24 +0300

    Unfinished anims

---
+++ Assets/Ceyda's Scene/Scripts/SkeletonAnimationStateControllers.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonAnimationStateControllers : MonoBehaviour
+{
+    Animator animator;
+
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+     //   Debug.Log(animator);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
